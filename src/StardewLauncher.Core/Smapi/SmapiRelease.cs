94a94a750faa6f50e5ae164d56a44de319a5abdf
Arc655743 Orange <1343766898@qq.com>
2026-09-12 20:53:15 +0800

    v1.0.0

--- src/StardewLauncher.Core/Smapi/SmapiRelease.cs
+++
@@ -1,4 +0,0 @@
-namespace StardewLauncher.Core.Smapi;
-
-/// <summary>SMAPI 的一个发布版本信息。</summary>
-public sealed record SmapiRelease(string Version, string DownloadUrl, string ReleasePageUrl, DateTimeOffset? PublishedAt);
